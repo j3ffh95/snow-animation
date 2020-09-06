5ca0716f4e51ec1381ad8b224361df1a78556565
j3ffh95 <j3ffh95@gmail.com>
2020-09-05 22:31:53 -0400

    Edit the style object

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function createSnowflake() {
   const snow_flake = document.createElement("i");
   snow_flake.classList.add("fas");
   snow_flake.classList.add("fa-snowflake");
+  snow_flake.style.left = Math.random() * window.innerWidth + "px";
 
   document.body.appendChild(snow_flake);
 }
