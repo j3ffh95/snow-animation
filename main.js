dbc26d0dbcf960c7dad974f86198b5ace55cb89e
j3ffh95 <j3ffh95@gmail.com>
2020-09-05 22:29:23 -0400

    Created function to create snow flake

---
+++ main.js
@@ -0,0 +1,9 @@
+function createSnowflake() {
+  const snow_flake = document.createElement("i");
+  snow_flake.classList.add("fas");
+  snow_flake.classList.add("fa-snowflake");
+
+  document.body.appendChild(snow_flake);
+}
+
+createSnowflake();
