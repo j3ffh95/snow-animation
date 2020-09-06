44894723819171572b2cfbf2e51f5856de6d719d
j3ffh95 <j3ffh95@gmail.com>
2020-09-05 22:38:01 -0400

    Added opacity and font size in js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function createSnowflake() {
   snow_flake.classList.add("fa-snowflake");
   snow_flake.style.left = Math.random() * window.innerWidth + "px";
   snow_flake.style.animationDuration = Math.random() * 3 + 2 + "s"; // Num between 2-5
+  snow_flake.style.opacity = Math.random();
+  snow_flake.style.fontSize = Math.random() * 10 + 10 + "px"; // Num between 10 - 20
 
   document.body.appendChild(snow_flake);
 
